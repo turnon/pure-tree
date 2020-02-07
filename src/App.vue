14d7e309be25fa607dbfe567aa10460565206342
ken <block24block@gmail.com>
2020-02-07 23:55:45 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <tree :msg="123"></tree>
+  </div>
+</template>
+
+<script>
+  import tree from "./components/pure-tree.vue"
+
+  export default {
+    components: {
+      tree
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ src/components/pure-tree.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<style scoped>
+</style>
